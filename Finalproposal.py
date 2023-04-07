bf28431f52c2a78379680601bf0d096f0c1aa5de
CNL2003 <121319207+CNL2003@users.noreply.github.com>
2023-04-06 23:34:57 -0400

    Update Finalproposal.py

--- Finalproposal.py
+++ Finalproposal.py
@@ -2,38 +2,51 @@
 
 ## What will (likely) be the title of your project?
 
-TODO
+Artifcial Intelligence Used to Improve Patient Care and Treatment 
 
 ## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
 
-TODO
+A code that allows doctors to input symptoms and patient history and return a treatment plan and/or diagnosis. 
 
 ## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
 
-TODO
+I hope to use this project as a means of learning more about AI, specifically machine learning. I hope to focus on a general type of illness (heart, immune, neurological, etc.)
+and import data from a database and have the code read each line of this data file. I'll then write a code that takes in multiple inputs based on questions about a patients symptoms 
+and patient history. It will utilize if-else statement and functions and will return a treatment plan for the patient. I will be using Python for the entirety of this project. 
 
 ## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
-
-TODO, if applicable
+I am not planning to combine this project with another course's. 
 
 ## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
 
-TODO, if applicable
+I am not planning to collaborate with anyone else. 
 
 ## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
 
 ### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
 
-TODO
+I would be proud to apply some type of method utilized in AI/machine learning to my project. I hope to at least accomplish a project that incorporate multiple aspects 
+of a patients history (sex, allergies, environment...) and some of the symptoms that may influence their treatment/diagnosis. 
 
 ### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
 
-TODO
+It would be very cool to have a code that reads multiple lines in a data file and incorporates it into the code so that there can be multiple symptoms and patient history 
+components considered when making a diagnosis/treatment plan. 
 
 ### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
 
-TODO
+The best outcome would be an app or website specifically for physicians to use that allows physicians to input symptoms and patient history and returns a possible diagnosis with 
+the most viable treatment plan. Going further than this, I think it would also be a great outcome for the physician to get multiple possibilities and then input more code to find the
+best treatment plan for a patient. Since there are multiple illnesses that have very similar symptoms, I think this may help a lot of physicians with a more efficient means of making diagnoses. 
+Additionally, although physicians go through years of schooling, there may sometimes be a case where a physician may need help remembering symptoms of an illness that they are looking to treat. 
+Something else that can be added to this code could be a code that takes in information from different tests and imaging. I'm curious as to how we can utilize computers to undestand these types of things. 
 
 ## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
 
-TODO
+I am currently doing a lot of research on machine learning and AI. I am specifically interested in neural networks and deep learning. I also think there may be a way for me to apply statistics to this code in
+order to see the probability a patient has a specific diagnosis and if other factors may need to be considered (lab tests, imaging...). Besides this, I am also researching the ways in which we can use AI to improve
+healthcare systems. Here are some of the articles and research papers I plan to read: 
+
+https://www.nature.com/articles/s41598-021-87171-5
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8950225/
+https://towardsdatascience.com/predicting-inpatient-length-of-stay-at-hospitals-using-python-big-data-304e79d8c008
