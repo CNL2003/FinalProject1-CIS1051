5a21a65e33fe3a04145439de7965f1c0367137ca
CNL2003 <121319207+CNL2003@users.noreply.github.com>
2023-04-07 23:51:12 -0400

    Update Finalproposal.py

--- Finalproposal.py
+++ Finalproposal.py
@@ -6,47 +6,80 @@ Artifcial Intelligence Used to Improve Patient Care and Treatment
 
 ## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
 
-A code that allows doctors to input symptoms and patient history and return a treatment plan and/or diagnosis. 
+The main purpose of my project is to explore aspects of machine learning and data analytics by using python and 
+other applications in order to apply this to issues in healthcare. In order to make my project as simple and specific 
+as possible, I hope to focus on common conditions treated in primary care settings. The end goal of my project will be to
+have a way for physicians to improve the efficiency and accuracy of their diagnoses of common conditions that may have similar
+symptoms. 
 
 ## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+As a premed student and a future doctor (hopefully), I am very interested in issues in healthcare and means of improving patient-to-physician
+interaction and care. It has been estimated that 1 in 3 misdiagnoses result in serious injury or death. This statistic was found in a study
+conducted by Johns Hopkins University in their Journal titled, "Diagnosis", which seeks to improve the accuracy of diagnoses. 
+My original interest in Python derived from the fact that I am a neuroscience student and I knew that computation could be used to undestand many 
+concepts in neuroscience. However, after doing a lot of research on AI, I became more interested in learning about different models of machine learning
+and how they can be utilized to address issues in healthcare. 
 
-I hope to use this project as a means of learning more about AI, specifically machine learning. I hope to focus on a general type of illness (heart, immune, neurological, etc.)
-and import data from a database and have the code read each line of this data file. I'll then write a code that takes in multiple inputs based on questions about a patients symptoms 
-and patient history. It will utilize if-else statement and functions and will return a treatment plan for the patient. I will be using Python for the entirety of this project. 
+Before I continue, I should state the obvious fact that there is no way that I'll have the ability to become an expert in machine learning and create a 
+code that can be applied to all conditions treated in primary care facillities. Thus, what I hope to do, is incorporate some of the standard concepts I learned
+in this introductory Python course and include concepts I learn from a few resources on machine learning and AI. The concepts I hope to include from the course include; 
+file reading, if/else statements, and boolean statements, to name a few. Regarding new concepts, I am currently working on object-oriented coding in my laboratory, and hope 
+to possibly incorporate the idea of classes into my code. Furthermore, as a supplement to my education, I plan to watch many videos on AI and machine learning and the types of 
+models that can be used to address issues in healthcare. 
+
+In order to execute my code, I will utilize different python libraries and modules. I will then utilize python to read a csv file that I have created/found and split each line so 
+that there are categories (sex, age, gender, symptoms, medical history, etc.). I will decide what categories to include once I finalize my research. Furthermore, I am going to read more about
+machine learning models, and will use one of these models to "train" the program how to understand each component of the file. I can then use this model to predict the accuracy of the diagnosis made. 
+Note, this is all very theoretical, and is strictly the BEST outcome. 
+
+What I hope my software will do, at least, is read a file, split the lines of the file, create a class, and run so that anyone can input elements into that class that will allow them to determine
+if their diagnosis is accurate or not. 
+
 
 ## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
-I am not planning to combine this project with another course's. 
 
 ## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
-
-I am not planning to collaborate with anyone else. 
 
 ## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
 
 ### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+When I spoke with Professor Rosen during the Zoom class, he said one of the main purposes of this project is to learn something. The thing that I am most interested in learning is how computer science can be 
+applied to my future career. The specific concept within computer science that I would like to learn is AI and Machine Learning models. This requires that I know something about statistics and/or linear algebra, so 
+there may be the possibility that I learn about this as well. 
 
-I would be proud to apply some type of method utilized in AI/machine learning to my project. I hope to at least accomplish a project that incorporate multiple aspects 
-of a patients history (sex, allergies, environment...) and some of the symptoms that may influence their treatment/diagnosis. 
+Therefore, a GOOD outcome for my final project would be finding some way to incorporate machine learning into some aspect of my project while also incorporating preexisting concepts from this course. 
+Another GOOD outcome would be to have a code that reads a file, split the lines of the file, and returns an output that determines the accurracy of a physicians diagnosis based on a class created with object-oriented coding.
 
 ### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
 
-It would be very cool to have a code that reads multiple lines in a data file and incorporates it into the code so that there can be multiple symptoms and patient history 
-components considered when making a diagnosis/treatment plan. 
+A BETTER outcome would be plotting the end result with a line of best fit, incorporating statistics into my code, and having a wider range of primary care conditions included in the data set that I analyzed. 
+Another BETTER outcome would be including treatment plans in the output. 
+The last BETTER outcome would be finding a way to incorporate patient history and a patients identity into the code when making considerations for their treatment plan/diagnosis. 
 
 ### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
 
-The best outcome would be an app or website specifically for physicians to use that allows physicians to input symptoms and patient history and returns a possible diagnosis with 
-the most viable treatment plan. Going further than this, I think it would also be a great outcome for the physician to get multiple possibilities and then input more code to find the
-best treatment plan for a patient. Since there are multiple illnesses that have very similar symptoms, I think this may help a lot of physicians with a more efficient means of making diagnoses. 
-Additionally, although physicians go through years of schooling, there may sometimes be a case where a physician may need help remembering symptoms of an illness that they are looking to treat. 
-Something else that can be added to this code could be a code that takes in information from different tests and imaging. I'm curious as to how we can utilize computers to undestand these types of things. 
+The BEST outcome would be to create a chatbot that can communicate with physicians and health care practicioners. This, however, would require me to input a lot of data and responses to certain questions, which feels very unrealistic with
+the time frame given. 
+Another BEST outcome would be to have more elements of machine learning incorporated into my project. This is a more realistic outcome. 
 
 ## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+Step 1- Research applications of AI in healthcare
+Step 2- Research machine learning and types of machine learning models
+Step 3- Research utilize machine learning for prediciting diagnoses of patients (preexisting models)
+Step 4- Find a data file that has classifications for primary care conditions
+Step 5- If step 4 fails, create own data set on primary care conditions based on research
+Step 6- Begin coding file reading part of code into Visual Studio Code
+Step 7- Create a class for diagnostic model in Visual Studio Code
+Step 8- Apply machine learning models to code 
+Step 9- Test code 
 
-I am currently doing a lot of research on machine learning and AI. I am specifically interested in neural networks and deep learning. I also think there may be a way for me to apply statistics to this code in
-order to see the probability a patient has a specific diagnosis and if other factors may need to be considered (lab tests, imaging...). Besides this, I am also researching the ways in which we can use AI to improve
-healthcare systems. Here are some of the articles and research papers I plan to read: 
+Here are some of the articles and research papers I plan to read: 
 
 https://www.nature.com/articles/s41598-021-87171-5
 https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8950225/
+https://www.itransition.com/machine-learning/healthcare
+https://www.upgrad.com/blog/how-to-make-chatbot-in-python/
+https://www.coursera.org/learn/intro-to-healthcare/lecture/v6hUs/the-challenge-of-rising-health-care-costs
+https://www.cedars-sinai.org/programs/primary-care/conditions.html
+https://pub.towardsai.net/machine-learning-algorithms-for-beginners-with-python-code-examples-ml-19c6afd60daa
 https://towardsdatascience.com/predicting-inpatient-length-of-stay-at-hospitals-using-python-big-data-304e79d8c008
